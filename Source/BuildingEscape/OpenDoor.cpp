08fbc4c34f76c0ec204ef3ac748a61a5921810fe
Dino Duvnjak <Dino Duvnjak>
2016-08-15 17:45:35 +0200

    Door rotation setuup

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto OwnerRot = GetOwner()->GetActorRotation();
+
+	UE_LOG(LogTemp, Warning, TEXT("rotation of the door is %s"), *OwnerRot);
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -21,7 +21,13 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay(); // super is an inheritance to an engine saying " Do this first in up inheritance"
 
-	UE_LOG(LogTemp, Warning, TEXT("this is my mesage to a runtime")); //Unreal loging
+	FString ObjectName = GetOwner()->GetName(); //get owner returns the pointer and then we call the method to get a name it return a string, you can look it on hover
+
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString(); // kada vraca pointer onda koristimo -> kada vraca objekat onda .
+
+	//Unreal log
+	// For loging with text we have to *dereference pointer because unreal expects TCHAR array and we need to covert it to a string and %s more info https://wiki.unrealengine.com/Logs,_Printing_Messages_To_Yourself_During_Runtime
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 
 	// ...
 	
